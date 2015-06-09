f50de6cf5274d9f7696adfd11c065198367f6de8
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-06-09 05:47:40 +0530

    Modify code to print time also.

--- check_condition.cpp
+++ check_condition.cpp
@@ -2,8 +2,9 @@
 // Check whether a function satisfies a given condition or not.
 // Has to be modified for the functions other than arity 3.
 //======================================================================
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -86,12 +87,14 @@ bool check_function()
 
 int main(int argc, char const *argv[])
 {
+	struct timeval tp_start, tp_end;
+
 	/* Input the function */
-	func[0][0][0] = 0;	func[0][1][0] = 1;
+	func[0][0][0] = 3;	func[0][1][0] = 1;
 	func[0][0][1] = 1;	func[0][1][1] = 1;
 
 	func[1][0][0] = 1;	func[1][1][0] = 1;
-	func[1][0][1] = 1;	func[1][1][1] = 1;
+	func[1][0][1] = 1;	func[1][1][1] = 0;
 
 	cout <<"f:"<<endl;
 	cout <<"    "<< func[0][0][0] <<"\t"<< func[0][1][0] <<endl;
@@ -99,7 +102,13 @@ int main(int argc, char const *argv[])
 	cout <<"    "<< func[1][0][0] <<"\t"<< func[1][1][0] <<endl;
 	cout <<"    "<< func[1][0][1] <<"\t"<< func[1][1][1] <<endl << endl;
 
+	gettimeofday(&tp_start, NULL);
+
 	cout << check_function() <<endl;
+
+	gettimeofday(&tp_end, NULL);
+    //printf("Total time: %lld microseconds\n", tp_end.tv_sec*1000000+tp_end.tv_usec - (tp_start.tv_sec*1000000+tp_start.tv_usec));
+    cout << "Total time: "<< tp_end.tv_sec*1000000+tp_end.tv_usec - (tp_start.tv_sec*1000000+tp_start.tv_usec) <<" microseconds"<< endl;
 
 	return 0;
 }
