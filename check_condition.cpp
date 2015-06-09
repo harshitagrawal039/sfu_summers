0beb645b9b5e2154a754ac5668079a8fbe4981e5
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-06-10 03:38:28 +0530

    Fix the indentation

--- check_condition.cpp
+++ check_condition.cpp
@@ -90,11 +90,11 @@ int main(int argc, char const *argv[])
 	struct timeval tp_start, tp_end;
 
 	/* Input the function */
-	func[0][0][0] = 3;	func[0][1][0] = 1;
-	func[0][0][1] = 1;	func[0][1][1] = 1;
+	func[0][0][0] = 0.9;	func[0][1][0] = 0.9;
+	func[0][0][1] = 0.3;	func[0][1][1] = 0.1;
 
-	func[1][0][0] = 1;	func[1][1][0] = 1;
-	func[1][0][1] = 1;	func[1][1][1] = 0;
+	func[1][0][0] = 0.3;	func[1][1][0] = 0.1;
+	func[1][0][1] = 0.3;	func[1][1][1] = 0.1;
 
 	cout <<"f:"<<endl;
 	cout <<"    "<< func[0][0][0] <<"\t"<< func[0][1][0] <<endl;
@@ -105,9 +105,9 @@ int main(int argc, char const *argv[])
 	gettimeofday(&tp_start, NULL);
 
 	cout << check_function() <<endl;
-
+	
 	gettimeofday(&tp_end, NULL);
-    //printf("Total time: %lld microseconds\n", tp_end.tv_sec*1000000+tp_end.tv_usec - (tp_start.tv_sec*1000000+tp_start.tv_usec));
+	//printf("Total time: %lld microseconds\n", tp_end.tv_sec*1000000+tp_end.tv_usec - (tp_start.tv_sec*1000000+tp_start.tv_usec));
     cout << "Total time: "<< tp_end.tv_sec*1000000+tp_end.tv_usec - (tp_start.tv_sec*1000000+tp_start.tv_usec) <<" microseconds"<< endl;
 
 	return 0;
